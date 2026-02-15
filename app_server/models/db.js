7bc52308ab4e1ec44b2b0ebaa538f3f02aaf0a08
kat-e8 <katlego.gagoopane@element8.co.za>
2026-02-15 22:54:25 +0200

    setting up heroku...

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose');
+require('./players');
+
+const dbURI = 'mongodb://localhost/scagent';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose connected to ${dbURI}`);
+});
