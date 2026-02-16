2e7dae6f2631a490ab4f6abda564fa154b520c0c
kat-e8 <katlego.gagoopane@element8.co.za>
2026-02-16 07:13:02 +0200

    added cloud db

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 require('./players');
 
+
+//connection string
 const dbURI = 'mongodb://katmint.dala-cirius.ts.net/scagent';
-mongoose.connect(dbURI);
+
+if(process.env.NODE_ENV === 'production') {
+    dbURI = 'mongodb+srv://heroku:K%40stx8909@cluster0.7hhl6hq.mongodb.net/ppApp';
+}
 
 mongoose.connection.on('connected', () => {
     console.log(`Mongoose connected to ${dbURI}`);
