3227c5cb8e8dda11ef6f2999dac1dd59fe7bc0df
kat-e8 <katlego.gagoopane@element8.co.za>
2026-02-17 01:18:31 +0200

    +about, route & controller

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,9 +1,9 @@
 const about = (req, res) => {
-    res.render('index', {title: "About"})
+    res.render('generic-text', {title: "About"})
 };
 
 const home = (req, res) => {
-    res.render('index', {title: "Home"})
+    res.render('index', {title: "Ayobayo!"})
 };
 
 module.exports = {
