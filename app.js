593d0ab0b671ac349bae49f4798c3751ba467762
kat-e8 <katlego.gagoopane@element8.co.za>
2026-02-16 06:55:55 +0200

    added .ico

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(403));
+  next(createError(404));
 });
 
 // error handler
